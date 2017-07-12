ac27a96a6d182a3d7e35036dc29afd4c22468e25
angelmmg90 <angelmmg90@hotmail.com>
2017-07-13 00:05:06 +0200

    #5 | Created event setOnClickListener and the sharedPreferences's file

--- app/src/main/java/com/fragmentos/seas/fragmentosysharedpreferences/LoginActivity.java
+++ app/src/main/java/com/fragmentos/seas/fragmentosysharedpreferences/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.fragmentos.seas.fragmentosysharedpreferences;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,8 +23,25 @@ public class LoginActivity extends AppCompatActivity {
         /*Objetivo: SharedPreferences*/
 
         edtUser = (EditText) findViewById(R.id.edtUser);
+        edtPass = (EditText) findViewById(R.id.edtPass);
 
+        btnEnviar = (Button) findViewById(R.id.btnEnviar);
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*Antes de pasar a la siguiente pantalla, guardo los datos en SharedPreferences*/
+                saveSharedPreferences();
 
+                /*Intent navegarEntrePantallas = new Intent(getBaseContext(), "??");
+                startActivity(navegarEntrePantallas);*/
+                /*Find*/
+
+            }
+        });
         /*Fin*/
+    }
+
+    private void saveSharedPreferences(){
+        SharedPreferences guardarEnXml = getSharedPreferences("DATOS_MUSICA", Context.MODE_PRIVATE);
     }
 }
