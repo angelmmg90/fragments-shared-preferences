2bf4746767cfde8d97154fe8f6609ef628bfe9c9
angelmmg90 <angelmmg90@hotmail.com>
2017-07-12 23:54:44 +0200

    #4 | Created two editText with labels and we get the reference in Java

--- app/src/main/java/com/fragmentos/seas/fragmentosysharedpreferences/LoginActivity.java
+++ app/src/main/java/com/fragmentos/seas/fragmentosysharedpreferences/LoginActivity.java
@@ -1,13 +1,26 @@
 package com.fragmentos.seas.fragmentosysharedpreferences;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText edtUser;
+    private EditText edtPass;
+    private Button btnEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        /*Objetivo: SharedPreferences*/
+
+        edtUser = (EditText) findViewById(R.id.edtUser);
+
+
+        /*Fin*/
     }
 }
